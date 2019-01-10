a203eb5d0b77760bf8b2ff05e3a7ab16c2742d1c
wanglijun93 <1402499306@qq.com>
2019-01-10 16:20:37 +0800

    #添加有状态的stateful #添加英语单词依赖

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,9 +13,24 @@ class MyApp extends StatelessWidget {
           title: new Text('welcome to flutter'),
         ),
         body: new Center(
-          child: new Text('hello flutter'),
+//          child: new Text('hello flutter'),
+          child: new RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    var wordPair = new WordPair.random();
+
+    return new Text(wordPair.asUpperCase);
   }
 }
