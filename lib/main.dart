abaebf88c000650e4e374a0c28991d00d07edd28
wanglijun93 <1402499306@qq.com>
2019-01-10 16:33:29 +0800

    #创建一个无限滚动ListView

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter Demo',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('welcome to flutter'),
-        ),
-        body: new Center(
-//          child: new Text('hello flutter'),
-          child: new RandomWords(),
-        ),
-      ),
+//      home: new Scaffold(
+//        appBar: new AppBar(
+//          title: new Text('welcome to flutter'),
+//        ),
+//        body: new Center(
+////          child: new Text('hello flutter'),
+//          child: new RandomWords(),
+//        ),
+//      ),
+      home: new RandomWords(),
     );
   }
 }
@@ -27,10 +28,41 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
-    var wordPair = new WordPair.random();
+//    var wordPair = new WordPair.random();
+//    return new Text(wordPair.asUpperCase);
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('英语单词'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
 
-    return new Text(wordPair.asUpperCase);
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return new Divider();
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(15));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
   }
 }
